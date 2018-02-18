2d183acd0d536939b2769cf1530638f2348adbb6
HFO4 <912394456@qq.com>
2018-02-18 19:46:43 +0800

    测试自动构建

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 zip -r -q -o pack.zip  ./
-curl -F "token=$TOKEN&commit=$TRAVIS_COMMIT" -F "filename=@pack.zip" -H "Expect:" http://2d7usb.natappfree.cc/t.php
+curl -F "token=$TOKEN" -F "commit=$TRAVIS_COMMIT" -F "filename=@pack.zip" -H "Expect:" http://cloudreve.org/deploy.php
--- t.php
+++
@@ -1,3 +0,0 @@
-<?php 
-error_log($_POST["token"]."|".$_POST["commit"]);
-?>
