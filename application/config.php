c4ecee4897c18bf903a5c0afcf4c2566e5956207
HFO4 <912394456@qq.com>
2018-04-06 21:24:56 +0800

    关闭Debug，修改错误提示

--- application/config.php
+++ application/config.php
@@ -18,7 +18,7 @@ return [
     // 应用命名空间
     'app_namespace'          => 'app',
     // 应用调试模式
-    'app_debug'              => true,
+    'app_debug'              => false,
     // 应用Trace
     'app_trace'              => false,
     // 应用模式状态
@@ -156,7 +156,7 @@ return [
     'exception_tmpl'         => THINK_PATH . 'tpl' . DS . 'think_exception.tpl',
 
     // 错误显示信息,非调试模式有效
-    'error_message'          => '页面错误！请稍后再试～',
+    'error_message'          => '出现错误,我们都有不顺利的时候，请坐和放宽',
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
