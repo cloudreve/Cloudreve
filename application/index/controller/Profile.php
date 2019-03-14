3a4912083c38e1b64b9c5a5aa88ee1d219287dd5
HFO4 <912394456@qq.com>
2019-03-14 10:00:53 +0800

    Feat: User profile

--- application/index/controller/Profile.php
+++ application/index/controller/Profile.php
@@ -14,11 +14,63 @@ class Profile extends Controller{
 	public $siteOptions;
 
 	public function _initialize(){
-		$this->siteOptions = Option::getValues(["basic"]);
+	}
+
+	public function getList(){
+		$userId = (string)input("post.uid");
+		$userData = Db::name("users")->where("id",$userId)->find();
+		$page = (int)input("post.page");
+		if (empty($userId) || empty($userData) || $userData["profile"] == 0){
+			$this->error('用户主页不存或者用户关闭了个人主页',404,$this->siteOptions);
+		   }
+		   switch (input("post.type")) {
+			case 'all':
+				$list = Db::name('shares')
+				->where('owner',$userId)
+				->where('type',"public")
+				->order('share_time DESC')
+				->page($page.',10')
+				->select();
+				break;
+			case 'hot':
+				$num = Option::getValue("hot_share_num");
+				$list = Db::name('shares')
+				->where('owner',$userId)
+				->where('type',"public")
+				->order('download_num DESC')
+				->limit($num)
+				->select();
+				break;
+			default:
+				$list = Db::name('shares')
+				->where('owner',$userId)
+				->where('type',"public")
+				->order('share_time DESC')
+				->page($page.',10')
+				->select();
+				break;
+		}
+		$listData = $list;
+		foreach ($listData as $key => $value) {
+			unset($listData[$key]["share_pwd"]);
+			unset($listData[$key]["source_name"]);
+			if($value["source_type"]=="file"){
+				$listData[$key]["fileData"] = Db::name('files')->where('id',$value["source_name"])->find()["orign_name"];
+
+			}else{
+				$pathDir = explode("/",$value["source_name"]);
+				$listData[$key]["fileData"] = end($pathDir);
+			}
+		}
+
+		return json($listData);
+
+
 	}
 
 	public function index(){
 		$this->visitorObj = new User(cookie('user_id'),cookie('login_key'));
+		$this->siteOptions = Option::getValues(["basic"],$this->visitorObj->userSQLData);
 		$userId = (string)input("param.uid");
 		$userData = Db::name("users")->where("id",$userId)->find();
 		if (empty($userId) || empty($userData) || $userData["profile"] == 0){
@@ -30,47 +82,14 @@ class Profile extends Controller{
 				->where('type',"public")
 				->count();
 		$regDays = (int)((time()-strtotime($userData["user_date"]))/86400);
-		switch (input("get.type")) {
-			case 'all':
-				$list = Db::name('shares')
-				->where('owner',$userId)
-				->where('type',"public")
-				->order('share_time DESC')
-				->paginate(10);
-				break;
-			case 'hot':
-				$num = Option::getValue("hot_share_num");
-				$list = Db::name('shares')
-				->where('owner',$userId)
-				->where('type',"public")
-				->paginate($num);
-				break;
-			default:
-				$list = Db::name('shares')
-				->where('owner',$userId)
-				->where('type',"public")
-				->order('share_time DESC')
-				->paginate(10);
-				break;
-		}
-		$listData = $list->all();
-		foreach ($listData as $key => $value) {
-			if($value["source_type"]=="file"){
-				$listData[$key]["fileData"] = Db::name('files')->where('id',$value["source_name"])->find()["orign_name"];
-
-			}else{
-				$pathDir = explode("/",$value["source_name"]);
-				$listData[$key]["fileData"] = end($pathDir);
-			}
-		}
+		
 		return view("profile",[
 			"options" => $this->siteOptions,
 			'loginStatus' => $this->visitorObj->loginStatus,
-			'userInfo' => $userData,
-			'userData' => $this->visitorObj->userSQLData,
+			'targetUserInfo' => $userData,
+			'userSQL' => $this->visitorObj->userSQLData,
+			'userInfo' => $this->visitorObj->getInfo(),
 			'groupData' => $groupData,
-			'list' => $listData,
-			'listOrigin' => $list,
 			'type' => input("get.type"),
 			'shareCount' => $shareCount,
 			'regDays' => $regDays,
