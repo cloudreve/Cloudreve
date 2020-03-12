36cec067d9dcac3bae203f616012b89f59d822de
HFO4 <912394456@qq.com>
2020-03-12 15:56:06 +0800

    Fix: OneDrive monitoring should continue when error occurs

--- pkg/filesystem/driver/onedrive/api.go
+++ pkg/filesystem/driver/onedrive/api.go
@@ -445,6 +445,7 @@ func (client *Client) MonitorUpload(uploadURL, callbackKey, path string, size ui
 					}
 				}
 				util.Log().Debug("无法获取上传会话状态，继续下一轮，%s", err.Error())
+				continue
 			}
 
 			// 成功获取分片上传状态，检查文件大小
