fa900b166a95827ee381e77ebe72f254cb50c76f
HFO4 <912394456@qq.com>
2020-04-08 10:00:03 +0800

    Feat: check static files version

--- bootstrap/static.go
+++ bootstrap/static.go
@@ -1,15 +1,23 @@
 package bootstrap
 
 import (
+	"encoding/json"
+	"github.com/HFO4/cloudreve/pkg/conf"
 	"github.com/HFO4/cloudreve/pkg/util"
 	_ "github.com/HFO4/cloudreve/statik"
 	"github.com/gin-contrib/static"
 	"github.com/rakyll/statik/fs"
+	"io/ioutil"
 	"net/http"
 )
 
 type GinFS struct {
 	FS http.FileSystem
+}
+
+type staticVersion struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
 }
 
 // StaticFS 内置静态文件资源
@@ -43,6 +51,40 @@ func InitStatic() {
 		if err != nil {
 			util.Log().Panic("无法初始化静态资源, %s", err)
 		}
+	}
+
+	// 检查静态资源的版本
+	f, err := StaticFS.Open("version.json")
+	if err != nil {
+		util.Log().Warning("静态资源版本标识文件不存在，请重新构建或删除 statics 目录")
+		return
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		util.Log().Warning("无法读取静态资源文件版本，请重新构建或删除 statics 目录")
+		return
+	}
+
+	var v staticVersion
+	if err := json.Unmarshal(b, &v); err != nil {
+		util.Log().Warning("无法解析静态资源文件版本, %s", err)
+		return
+	}
+
+	staticName := "cloudreve-frontend"
+	if conf.IsPro == "true" {
+		staticName += "-pro"
+	}
+
+	if v.Name != staticName {
+		util.Log().Warning("静态资源版本不匹配，请重新构建或删除 statics 目录")
+		return
+	}
+
+	if v.Version != conf.RequiredStaticVersion {
+		util.Log().Warning("静态资源版本不匹配 [当前 %s, 需要: %s]，请重新构建或删除 statics 目录", v.Version, conf.RequiredStaticVersion)
+		return
 	}
 
 }
