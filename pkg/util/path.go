b2ddac9abc04211df02cadb7df455a5c73b1066a
不帅你报警 <yrjyrjwp7@hotmail.com>
2021-10-29 11:24:43 +0800

    相对路径切换为相对工作目录而不是可执行文件

--- pkg/util/path.go
+++ pkg/util/path.go
@@ -48,11 +48,11 @@ func FormSlash(old string) string {
 	return path.Clean(strings.ReplaceAll(old, "\\", "/"))
 }
 
-// RelativePath 获取相对可执行文件的路径
+// RelativePath 获取相对工作目录的路径
 func RelativePath(name string) string {
 	if filepath.IsAbs(name) {
 		return name
 	}
-	e, _ := os.Executable()
-	return filepath.Join(filepath.Dir(e), name)
+	e, _ := os.Getwd()
+	return filepath.Join(e, name)
 }
