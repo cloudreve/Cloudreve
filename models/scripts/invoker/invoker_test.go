4dbe8670209f5df0be567767bc82d855866557f9
HFO4 <912394456@qq.com>
2021-11-22 20:38:03 +0800

    Fix: failed unit test due to import cycle

--- models/scripts/invoker/invoker_test.go
+++ models/scripts/invoker/invoker_test.go
@@ -2,35 +2,14 @@ package invoker
 
 import (
 	"context"
-	"database/sql"
-	"github.com/DATA-DOG/go-sqlmock"
-	model "github.com/cloudreve/Cloudreve/v3/models"
-	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-var mock sqlmock.Sqlmock
-var mockDB *gorm.DB
 
 type TestScript int
 
 func (script TestScript) Run(ctx context.Context) {
 
-}
-
-// TestMain 初始化数据库Mock
-func TestMain(m *testing.M) {
-	var db *sql.DB
-	var err error
-	db, mock, err = sqlmock.New()
-	if err != nil {
-		panic("An error was not expected when opening a stub database connection")
-	}
-	model.DB, _ = gorm.Open("mysql", db)
-	mockDB = model.DB
-	defer db.Close()
-	m.Run()
 }
 
 func TestRunDBScript(t *testing.T) {
