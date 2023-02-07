a93ea2cfa05953baeb94b46379a1c14020ba6dba
WeidiDeng <weidi_deng@icloud.com>
2023-02-07 19:43:28 +0800

    feat(webdav): add read-only option (#1629)

--- service/setting/webdav.go
+++ service/setting/webdav.go
@@ -22,6 +22,12 @@ type WebDAVAccountCreateService struct {
 	Name string `json:"name" binding:"required,min=1,max=255"`
 }
 
+// WebDAVAccountUpdateReadonlyService WebDAV 修改只读性服务
+type WebDAVAccountUpdateReadonlyService struct {
+	ID       uint `json:"id" binding:"required,min=1"`
+	Readonly bool `json:"readonly"`
+}
+
 // WebDAVMountCreateService WebDAV 挂载创建服务
 type WebDAVMountCreateService struct {
 	Path   string `json:"path" binding:"required,min=1,max=65535"`
@@ -54,6 +60,14 @@ func (service *WebDAVAccountCreateService) Create(c *gin.Context, user *model.Us
 func (service *WebDAVAccountService) Delete(c *gin.Context, user *model.User) serializer.Response {
 	model.DeleteWebDAVAccountByID(service.ID, user.ID)
 	return serializer.Response{}
+}
+
+// Update 修改WebDAV账户的只读性
+func (service *WebDAVAccountUpdateReadonlyService) Update(c *gin.Context, user *model.User) serializer.Response {
+	model.UpdateWebDAVAccountReadonlyByID(service.ID, user.ID, service.Readonly)
+	return serializer.Response{Data: map[string]bool{
+		"readonly": service.Readonly,
+	}}
 }
 
 // Accounts 列出WebDAV账号
