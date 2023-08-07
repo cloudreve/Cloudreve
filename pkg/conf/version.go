ce832bf13d8372a84b993b8f65447bce6eced90a
Aaron Liu <912394456@qq.com>
2023-08-07 20:09:23 +0800

    release: 3.8.2

--- pkg/conf/version.go
+++ pkg/conf/version.go
@@ -1,10 +1,10 @@
 package conf
 
 // BackendVersion 当前后端版本号
-var BackendVersion = "3.8.1"
+var BackendVersion = "3.8.2"
 
 // RequiredDBVersion 与当前版本匹配的数据库版本
-var RequiredDBVersion = "3.8.0-beta1"
+var RequiredDBVersion = "3.8.1"
 
 // RequiredStaticVersion 与当前版本匹配的静态资源版本
 var RequiredStaticVersion = "3.8.1"
